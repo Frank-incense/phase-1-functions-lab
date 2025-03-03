49e95bca5e231b645e8a8bf3bffa7d07b6942d90
Frankincense <frankincensewelsey@gmail.com>
2025-03-03 16:11:59 +0300

    creating functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    return Math.abs(distance - 42);
+}
+function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance) * 264;
+}
+function distanceTravelledInFeet(start, finish) {
+    let blocks = start - finish;
+    let feet = blocks * 264;
+    return Math.abs(feet);
+}
+function calculatesFarePrice(start, finish) {
+    let distance = distanceTravelledInFeet(start, finish);
+    let fare = 0;
+    if (distance > 2500){
+        return 'cannot travel that far';
+    }
+    else if (distance > 2000) {
+        return 25;
+    }
+    else if (distance > 400) {
+        fare = (distance - 400) * 2;
+        return fare / 100;
+    }
+    else {
+        return 0;
+    }
+
+}
